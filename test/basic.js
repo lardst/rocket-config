2377b6e8272a7d80e5b8c41e14ed1f36966ffce8
Burnett, Lawrence <Lawrence.Burnett@disney.com>
2017-09-22 10:21:17 -0700

    Misc changes
    Finished the test file.
    Restructured the test configs.
    Added error logging to log file.

--- test/basic.js
+++ test/basic.js
@@ -1,8 +1,8 @@
 var mocha = require("mocha"),
-		should = require("should"),
-		rocket = require("../")('./test/config/');
+    should = require("should"),
+    rocket = require("../")('./test/config/');
 
-describe("Rocket config Method Tests.", function () {
+describe("Rocket config method tests.", function () {
     it("The 'config' data should be an object.", function (done) {
         rocket.config.should.be.Object
         done();
@@ -10,6 +10,22 @@ describe("Rocket config Method Tests.", function () {
 
     it("The 'config' data should have a property 'groot' with a value of 'We are Groot'.", function (done) {
         rocket.config.should.have.property('groot', 'We are Groot');
+        done();
+    });
+
+    it("The 'global' data should have a property 'doctor' with a value of 'who'.", function (done) {
+        rocket.global.should.have.property('doctor', 'who');
+        done();
+    });
+
+    it("The 'global' data should have a property 'ddalek.exterminate.exterminate.exterminate' that is a boolean.", function (done) {
+        rocket.global.dalek.exterminate.exterminate.exterminate.should.be.Boolean;
+        done();
+    });
+
+    it("Using the 'stage' environment there should be a 'gamora' property.", function (done) {
+        rocket = require("../")('./test/config/', 'stage')
+        rocket.config.should.have.property('gamora');
         done();
     });
 });
